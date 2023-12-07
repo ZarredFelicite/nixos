fb919e8374d0f878ffeb063ec288e48736a2bda9
ZarredFelicite <zarred.f@gmail.com>
2023-12-08 08:51:30 +1100

    swaync -> mako

--- home/desktop/notifications.nix
+++ home/desktop/notifications.nix
@@ -3,15 +3,15 @@
     actions = true;
     anchor = "top-right";
     backgroundColor = lib.mkForce "#19172480";
-    borderColor = lib.mkForce "#9ccfd8";
+    borderColor = lib.mkForce "#9ccfd880";
     textColor = lib.mkForce "#e0def4";
-    borderRadius = 10;
-    borderSize = 2;
+    borderRadius = 12;
+    borderSize = 1;
     defaultTimeout = 5000;
-    format = "%s\\n%b";
+    format = "<b>%s</b>\\n%b";
     groupBy = null;
-    height = 200;
-    width = 300;
+    height = 1000;
+    width = 500;
     icons = true;
     layer = "overlay";
     margin = "4";
@@ -20,5 +20,8 @@
     maxVisible = 6;
     padding = "4";
     progressColor = lib.mkForce "over #26233a";
+    extraConfig = ''
+      outer-margin=4
+    '';
   };
 }

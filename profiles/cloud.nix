39c40bf2ba838ab49b84ce135c4ad182a1e5c02b
ZarredFelicite <zarred.f@gmail.com>
2024-01-05 11:17:17 +1100

    upgrade nextcloud to v28

--- profiles/cloud.nix
+++ profiles/cloud.nix
@@ -1,14 +1,14 @@
 { pkgs, config, ... }: {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud27;
+    package = pkgs.nextcloud28;
     hostName = "nextcloud.zar.red";
     https = true;
     home = "/var/lib/nextcloud";
     datadir = config.services.nextcloud.home;
+    extraOptions.trusted_domains = [ "localhost" ];
     config = {
       adminpassFile = config.sops.secrets.nextcloud-admin.path;
-      extraTrustedDomains = [ "localhost" ];
     };
     database.createLocally = true;
   };

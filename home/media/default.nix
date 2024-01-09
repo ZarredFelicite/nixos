78e3ea29f3fea013cfb960f60108637dc2e616bc
ZarredFelicite <zarred.f@gmail.com>
2024-01-09 19:59:33 +1100

    configure btop with networked drives

--- home/media/default.nix
+++ home/media/default.nix
@@ -20,6 +20,29 @@
       port = config.services.mpd.network.port;
     };
     settings = {
+      directory = "/mnt/gargantua/media/music";
+      library = "/mnt/gargantua/media/music/data/beets.db";
+      import = {
+        move = false;
+        write = false;
+      };
+      paths = {
+        default = "%lower{$albumartist}/%lower{$album%aunique{}}/$track - %lower{$title}";
+        singleton = "%lower{$artist}/%lower{$title}";
+        comp = "Compilations/%lower{$album%aunique{}}/$track %lower{$title}";
+      };
+      plugins = [ "fetchart" "edit" "scrub" "lyrics" "acousticbrainz" "zero" "info" ];
+      zero = {
+        auto = false;
+        fields = [ "albumtype" "albumtypes" ];
+        update_database = true;
+        #fetchart:
+        #discogs:
+        #   source_weight: 0.0
+        #musicbrainz:
+        #   enabled: no
+        #   source_weight: 0.8
+      };
     };
   };
 }

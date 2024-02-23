cd20ecc64a3e8f0e62781281e78270d35b6416dd
ZarredFelicite <zarred.f@gmail.com>
2024-02-24 10:23:02 +1100

    tridactyl and nixvim configuration

--- home/cli/nixvim.nix
+++ home/cli/nixvim.nix
@@ -65,7 +65,10 @@ imports = [
           { name = "nvim_lsp"; }
           { name = "luasnip"; }
           { name = "path"; }
-          { name = "buffer"; }
+          {
+            name = "buffer";
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
           { name = "cmdline"; }
           { name = "cmdline_history"; }
           { name = "cmp-latex-symbols"; }
@@ -73,18 +76,44 @@ imports = [
           { name = "tmux"; }
           { name = "treesitter"; }
         ];
+        snippet.expand = "luasnip";
+        completion.keywordLength = 2;
         mapping = {
           "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Up>" = "cmp.mapping.scroll_docs(4)";
+          "<S-Down>" = "cmp.mapping.scroll_docs(-4)";
+          "<Esc>" = "cmp.mapping.close()";
+          "<S-Tab>" = {
+            action = ''
+              function(fallback)
+                local luasnip = require('luasnip')
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end
+            '';
+            modes = [ "i" "s" ];
+          };
           "<Tab>" = {
             action = ''
               function(fallback)
+                local luasnip = require('luasnip')
+                local has_words_before = function()
+                  unpack = unpack or table.unpack
+                  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+                end
                 if cmp.visible() then
                   cmp.select_next_item()
                 elseif luasnip.expandable() then
                   luasnip.expand()
                 elseif luasnip.expand_or_jumpable() then
                   luasnip.expand_or_jump()
-                elseif check_backspace() then
+                elseif has_words_before() then
                   fallback()
                 else
                   fallback()
@@ -106,7 +135,7 @@ imports = [
         };
       };
       luasnip.enable = true;
-      cmp_luasnip.enable = true;
+      #cmp_luasnip.enable = true;
       treesitter = {
         enable = true;
         indent = true;
--- home/core.nix
+++ home/core.nix
@@ -99,6 +99,7 @@
         python-mpv-jsonipc
         playsound
         gtts
+        flask
         (
           buildPythonPackage rec {
             pname = "reader";

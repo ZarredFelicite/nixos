7c0aa3adfb05213bf17230920b0603ef322b0f9a
ZarredFelicite <zarred.f@gmail.com>
2024-08-10 14:08:23 +1000

    disable nixvim

--- home/cli/nixvim.nix
+++ home/cli/nixvim.nix
@@ -59,7 +59,7 @@ imports = [
         #theme = "palenight";
       };
       cmp = {
-        enable = true;
+        enable = false;
         settings = {
           sources = [
             { name = "nvim_lsp"; }
@@ -120,7 +120,7 @@ imports = [
         };
       };
       lsp = {
-        enable = true;
+        enable = false; #TODO
         servers = {
           nil_ls.enable = true;
           pyright.enable = true;
@@ -129,11 +129,16 @@ imports = [
           ruff-lsp.enable = true;
         };
       };
-      luasnip.enable = true;
+      luasnip.enable = false; #TODO
       #cmp_luasnip.enable = true;
       treesitter = {
-        enable = true;
+        enable = false; #TODO
         indent = true;
+        nixGrammars = false;
+        grammarPackages = [
+          #TODO complete list
+          pkgs.vimPlugins.nvim-treesitter-parsers.python
+        ];
       };
       telescope = {
         enable = true;

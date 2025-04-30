4d43fcbbb22cf9d7a62b00b002b1e68c47f2c83a
ZarredFelicite <zarred.f@gmail.com>
2025-04-30 22:48:51 +1000

    fix hyprland animations

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -153,14 +153,15 @@ in {
       };
       animations = {
        enabled = true;
-       bezier = "overshot,0.05,0.9,0.1,1.1";
+       bezier = "overshot,0.1,0.95,0.2,1.05";
        animation = [
-          "windowsIn, 1, 6, overshot, slide"
-          "windowsOut, 1, 6, overshot, slide"
-          "border, 1, 10, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-          "specialWorkspace, 1, 6, overshot"
+          # animation = NAME, ONOFF, SPEED, CURVE [,STYLE]
+          "windows, 1, 4, default, slide"
+          "layers, 1, 2, default, slide"
+          "fade, 1, 5, default"
+          "border, 1, 6, default"
+          "workspaces, 1, 4, default, slide"
+          "specialWorkspace, 1, 4, default, slidevert"
         ];
       };
       dwindle = {

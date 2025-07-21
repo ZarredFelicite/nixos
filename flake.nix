b58fe0153fb11f2e572f16f0ee2e348553d1d7a4
ZarredFelicite <zarred.f@gmail.com>
2025-07-21 23:21:20 +1000

    move unstable flake to nixos-unstable-small for faster updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Zarred's NixOS flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
     home-manager = { url = "github:nix-community/home-manager/release-25.05"; inputs.nixpkgs.follows = "nixpkgs"; };
     nur = { url = "github:nix-community/NUR"; };
     flake-utils.url = "github:numtide/flake-utils";

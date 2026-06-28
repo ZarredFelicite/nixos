107d4225b7c2e2b8d98305dc54d1d2699f66518a
ZarredFelicite <zarred.f@gmail.com>
2026-06-28 18:32:49 +1000

    fix(nixvim): stop clearing kitty images in tmux

--- home/cli/nixvim/default.nix
+++ home/cli/nixvim/default.nix
@@ -211,8 +211,8 @@ imports = [
           },
           max_width_window_percentage = 90,
           max_height_window_percentage = 50,
-          window_overlap_clear_enabled = true,
-          tmux_show_only_in_active_window = true,
+          window_overlap_clear_enabled = false,
+          tmux_show_only_in_active_window = false,
         })
 
         local function render_obsidian_image_embeds()

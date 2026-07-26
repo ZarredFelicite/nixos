9f9106b6ff9ba939469615a661247186ecca689b
ZarredFelicite <zarred.f@gmail.com>
2026-07-26 15:37:11 +1000

    fix(sankara): avoid duplicate WebSocket headers

--- profiles/nginx.nix
+++ profiles/nginx.nix
@@ -582,8 +582,6 @@
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header X-Forwarded-Host $http_host;
               proxy_set_header X-Forwarded-Proto https;
-              proxy_set_header Connection $connection_upgrade;
-              proxy_set_header Upgrade $http_upgrade;
 
               proxy_buffering off;
               proxy_cache off;

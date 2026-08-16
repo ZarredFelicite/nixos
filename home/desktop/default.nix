8b77973ef146df50ab3fdc21cf2544d1edd74eab
ZarredFelicite <zarred.f@gmail.com>
2026-08-16 19:32:06 +1000

    fix(cad): point OrcaSlicer desktop entry to unstable

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -269,7 +269,7 @@ in {
     genericName = "3D Printing Software";
     icon = "OrcaSlicer";
     # Let GLVND select the working OpenGL/EGL vendor on the multi-GPU host.
-    exec = "${pkgs.orca-slicer}/bin/orca-slicer";
+    exec = "${pkgs-unstable.orca-slicer}/bin/orca-slicer";
     terminal = false;
     type = "Application";
     mimeType = ["model/stl" "model/3mf" "application/vnd.ms-3mfdocument" "application/prs.wavefront-obj" "application/x-amf" "x-scheme-handler/orcaslicer"];
